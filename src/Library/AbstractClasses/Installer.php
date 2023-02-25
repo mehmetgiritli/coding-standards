e171fd226b85eddb050d170e69298d0ec20c0111
Mehmet Giritli <mehmet.giritli@abdn.ac.uk>
2023-02-25 00:40:01 +0000

    Move file

--- src/Library/AbstractClasses/Installer.php
+++ src/Library/AbstractClasses/Installer.php
@@ -1,12 +1,13 @@
 <?php
 
-namespace Library;
+namespace Library\AbstractClasses;
 
 use Composer\Script\Event;
 use Library\Messages;
 
-class Installer
+abstract class Installer
 {
+
     public static function start(Event $event)
     {
         self::movePayloads($event);
@@ -19,9 +20,9 @@ class Installer
         $workDirectory = getcwd();
 
         if (PHP_OS_FAMILY === 'Windows') {
-            $command = sprintf("rd /s /q %s", $workDirectory);
+            $command = sprintf('rd /s /q %s', $workDirectory);
         } else {
-            $command = sprintf("rm -rf %s", $workDirectory);
+            $command = sprintf('rm -rf %s', $workDirectory);
         }
 
         exec($command);
@@ -29,16 +30,7 @@ class Installer
 
     private static function movePayloads(Event $event)
     {
-        $payloads = [
-            'vscode/.vscode'                     => '.vscode',
-            'standards/javascript/.eslintrc.js'  => '.eslintrc.js',
-            'standards/sass/stylelint.config.js' => 'stylelint.config.js',
-            'standards/php'                      => 'php-coding-standards',
-            'editor/.gitattributes'              => '.gitattributes',
-            'editor/.editorconfig'               => '.editorconfig'
-        ];
-
-        foreach ($payloads as $source => $target) {
+        foreach (static::$payloads as $source => $target) {
             if (!is_int($source)) {
                 $sourceFileName = $source;
             } else {
@@ -49,7 +41,6 @@ class Installer
             $targetFileName = "../{$target}";
 
             if (file_exists($targetFileName) && getenv('OVERWRITE') != true) {
-
                 $message = "{$target} exists, not overwriting.";
                 $formattedMessage = Messages::getComment($message);
                 Messages::display($event, $formattedMessage);
@@ -71,13 +62,9 @@ class Installer
         $formattedMessage = Messages::getInfo($message);
         Messages::display($event, $formattedMessage);
 
-        $packages = [
-            'squizlabs/php_codesniffer',
-            'dealerdirect/phpcodesniffer-composer-installer',
-            'sirbrillig/phpcs-variable-analysis',
-        ];
 
-        $composerParameters = implode(' ', $packages);
+
+        $composerParameters = implode(' ', static::$packages);
 
         $workDir = getcwd() . '/../';
         $command = "composer --dev require {$composerParameters} -d {$workDir}";
@@ -90,25 +77,12 @@ class Installer
         $formattedMessage = Messages::getInfo($message);
         Messages::display($event, $formattedMessage);
 
-        $nodePackages = [
-            'eslint',
-            'eslint-plugin-import',
-            'eslint-plugin-n',
-            'eslint-plugin-promise',
-            'eslint-config-standard',
-            'eslint-plugin-vue',
-            '@html-eslint/parser',
-            '@html-eslint/eslint-plugin',
-            'stylelint',
-            'stylelint-config-standard',
-            'postcss@8',
-            'postcss-scss'
-        ];
 
-        $npmPackageParameters = implode(' ', $nodePackages);
+
+        $npmPackageParameters = implode(' ', static::$nodePackages);
         $workDir = getcwd() . '/../';
 
-        $command = "npm install --cwd {$workDir} --save-dev {$npmPackageParameters}";
+        $command = "npm install --prefix {$workDir} --save-dev {$npmPackageParameters}";
         exec($command);
     }
 }
